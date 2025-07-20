30463091594e8a25be10221dce0625c5f39742fb
Rithik Gautham G <zoneofresilience07@gmail.com>
2025-07-20 20:49:38 +0530

    Add files via upload

---
+++ EXP 03/Output_3.py
@@ -0,0 +1,15 @@
+Python 3.12.9 (tags/v3.12.9:fdb8142, Feb  4 2025, 15:27:58) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/tgane/OneDrive/Desktop/5th sem/dlt/Exp 3/Exp 3.py =====
+Epoch 0, Loss: 0.6935
+Epoch 100, Loss: 0.0138
+Epoch 200, Loss: 0.0052
+Epoch 300, Loss: 0.0026
+Epoch 400, Loss: 0.0017
+Epoch 500, Loss: 0.0012
+Epoch 600, Loss: 0.0009
+Epoch 700, Loss: 0.0007
+Epoch 800, Loss: 0.0006
+Epoch 900, Loss: 0.0005
+Test Accuracy: 97.37%
