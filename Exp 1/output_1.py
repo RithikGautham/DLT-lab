d18fca75973ea8d99ec8fdff7a93de7839844399
Rithik Gautham G <zoneofresilience07@gmail.com>
2025-07-20 20:40:17 +0530

    Add files via upload

---
+++ Exp 1/output_1.py
@@ -0,0 +1,31 @@
+Python 3.12.9 (tags/v3.12.9:fdb8142, Feb  4 2025, 15:27:58) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:\Users\tgane\OneDrive\Desktop\5th sem\dlt\exp 1\EXP no 1.py ====
+AND
+(0, 0) -> 0
+(0, 1) -> 0
+(1, 0) -> 0
+(1, 1) -> 1
+
+OR
+(0, 0) -> 0
+(0, 1) -> 1
+(1, 0) -> 1
+(1, 1) -> 1
+
+NOT
+0 -> 1
+1 -> 0
+
+NOR
+(0, 0) -> 1
+(0, 1) -> 0
+(1, 0) -> 0
+(1, 1) -> 0
+
+XOR
+(0, 0) -> 0
+(0, 1) -> 1
+(1, 0) -> 1
+(1, 1) -> 0
